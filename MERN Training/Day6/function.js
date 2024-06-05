4150b4bce4d73806dd4c96c0c39794fdaefeeaa8
Normi333 <shisirthapa146@gmail.com>
2024-06-05 17:35:19 +0545

    functions

---
+++ MERN Training/Day6/function.js
@@ -0,0 +1,61 @@
+/*function greet(name, phrase){
+    phrase = phrase ?? "Hello"
+    console.log(`hello ${name}`);
+}*/
+
+// function can be treated as a variable. can be assigned as a variable. can be passed as an aruguement. can be assigned to other function. can be called from other function.
+//highorderfunction bhaneko bhitra as an arguement ma aru function call garda hunxa.
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// greet("Shisir");
+
+// let sayhi = function(){
+//     console.log("Hello")
+// }
+
+// let sayhello = sayhi
+// sayhi()
+// sayhello()
+
+//example of highorderfunction
+
+// function highOrderFunc(callBackFunction){
+//     callBackFunction();
+
+//     let returnedFunc = function(){
+//         console.log("test");
+//     };
+//     return test;
+// }
+
+// const returnedFunc = highOrderFunc();
+// returnedFunc();
+
+// arrow function
+
+// let add = (a,b) => {
+//     return a + b;
+// }
+
+// let add = (a,b) => a + b;
+
+// let sayheloo = name => {
+//     console.log(`heloo${name}`);
+// };
+// sayheloo()
+
+// exercise
+
+let ask = (question, yes, no) =>{
+    if(confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do You Agree?",
+    () => alert("You Agreed."),
+    () => alert("You canceled the execution.")
+);
