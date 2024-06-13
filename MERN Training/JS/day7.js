60955a671adc2765e392b5767283c3528df507ba
Normi333 <shisirthapa146@gmail.com>
2024-06-13 17:59:46 +0545

    exercise

--- MERN Training/JS/day7.js
+++ MERN Training/JS/day7.js
@@ -28,22 +28,87 @@
 
 //function constructor
 
-function Person(name,age){
-    this.name = name;
-    this.age = age;
+// function Person(name,age){
+//     //this = {}
+//     this.name = name;
+//     this.age = age;
+//     //return {};
+//     // this.sayHello = () => console.log("Hello!"); esle chahi memory dher khanxa tei bhayera we don't do this here
+// }
 
-    // this.sayHello = () => console.log("Hello!"); esle chahi memory dher khanxa tei bhayera we don't do this here
+// Person.prototype.sayHello = function(){
+//     // console.log("hello!");
+//     console.log(this);
+// }
+
+// const shisir = new Person("Shisir", 22);
+// shisir.sayHello();
+
+// const raku = new Person('Raku', 23);
+// raku.sayHello();
+
+// console.log(shisir);
+// console.log(raku);
+
+// function Student(name, age, school){
+//     //this = {}
+//     Person.call(this, name, age)
+//     this.school = school;
+//     //this
+// }
+
+// Object.setPrototypeOf(Student.prototype, Person.prototype)
+
+// Student.prototype.getSchool = function(){
+//     console.log(this.school);
+// };
+
+// const riya = new Student('Riya', 23, 'blaa')
+// riya.sayHello();
+// riya.getSchool();
+
+// class Person{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     sayHello(){
+//         console.log(`${this.name}:Hello`);
+//     }
+// };
+
+// class Student extends Person{
+//     constructor(name, age, school){
+//         super(name, age); // Person.call(this, name, age)
+//         this.school = school;
+//     }
+
+//     getSchool(){
+//         console.log(this.school);
+//     }
+// };
+
+// const shisir = new Student('Shisir', 22, 'LFES')
+// shisir.sayHello()
+// console.log(shisir);
+
+
+//classwork
+
+class Book{
+    #title;
+    #author;
+    #genre;
+    #pages;
+    constructor(title,author,genre,pages){
+        this.#title = title;
+        this.#author = author;
+        this.#genre = genre;
+        this.#pages = pages;
+    }
+
+    displayInfo(){
+        console.log(`${this.#title}`)
+    }
 }
-
-Person.prototype.sayHello = function(){
-    console.log("hello!");
-}
-
-const shisir = new Person("Shisir", 22);
-shisir.sayHello();
-
-const raku = new Person('Raku', 23);
-raku.sayHello();
-
-console.log(shisir);
-console.log(raku);
