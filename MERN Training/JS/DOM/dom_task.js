f1e9f91a6fd3fa653678ebfc4fa8efbbff24221d
Normi333 <shisirthapa146@gmail.com>
2024-07-03 17:56:54 +0545

    Update

--- MERN Training/JS/DOM/dom_task.js
+++ MERN Training/JS/DOM/dom_task.js
@@ -6,55 +6,66 @@
 // 4. when delete button is clicked, that element is removed from the list.
 
 const form = document.querySelector('form');
+let taskToBeEditedList = null;
 let tasks = ["learn html", "learn js", "learn everything"];
 // create, read, update, delete
 function addTask(task){
   let ul = document.getElementById('ul');
   let li = document.createElement('li');
-  li.innerHTML= `${task}<button>delete</button>`;
+  li.innerHTML= `${task} <button>delete</button><button>edit</button>`;
   ul.appendChild(li);
 }
 
 function readTask(){
   let string="";
   const ul = document.querySelector('ul');
+  const deleteButton = "<button>delete</button><button>edit</button></li>";
   for(t of tasks){
-    str= `<li>${t}</li><button>delete</button>`;
+    str = `<li>${t}`.concat(deleteButton);
     string += str;
   }
   ul.innerHTML= string;
 }
 
-function updateTask(index, task){
-  tasks[index]=task;
+function updateTask(){
+  const updateTaskValue = document.getElementById("task").value;
+  taskToBeEditedList.innerHTML = `${updateTaskValue}<button>delete</button><button>edit</button>`;
+  document.querySelector("form").reset();
+  document.getElementById("submit").value = "Add";
+  taskToBeEditedList = null;
 }
 
 function removeTask(index){
   tasks.splice(index,1);
 }
 
-//event listener
-
 form.addEventListener('submit', (e)=>{
-  alert("Added!");
   e.preventDefault();
-  const newTask = document.getElementById("task").value;
-  const list = document.createElement("li"); //<li><li>
-  list.innerHTML = `${newTask}<button>delete</button>`; //<li>Learn Java</li>
-  document.querySelector("ul").append(list);
-  document.querySelector("form").reset();
-  //document.getElementById("task").value="";
+  const task=document.querySelector('#task').value;
+  if(taskToBeEditedList)updateTask();
+  else addTask(task);
+  document.getElementById('task').value=''; //documet.querySelector("form").reset()
 })
+
+const parentUl = document.getElementById('ul');
+
+parentUl.addEventListener('click', (e)=>{
+  // console.log(e);
+  const { tagName, textContent, parentElement } = e.target;
+  if (tagName === "BUTTON" && textContent === "delete"){
+    parentElement.remove();
+  }
+
+  if(tagName === "BUTTON" && textContent === "edit"){
+    console.log(parentElement.textContent);
+    const taskToBeUpdated = parentElement.textContent.replace("edit","").replace("delete","");
+    document.getElementById("task").value = taskToBeUpdated;
+    document.getElementById("submit").value = "Update";
+    taskToBeEditedList = parentElement;
+  }
+});
+
 
 readTask();
-
-const parentUI = document.querySelector('ul');
-
-parentUI.addEventListener('click',(e) => {
-  alert("Deleted!");
-  if(e.target.tagName === 'BUTTON'){
-      e.target.parentElement.remove()
-    }
-})
 
 
