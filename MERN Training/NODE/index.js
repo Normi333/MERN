f4ae5a088578eae2298193d82e4128207e57ad6d
Normi333 <shisirthapa146@gmail.com>
2024-07-09 17:14:02 +0545

    EDIT

--- MERN Training/NODE/index.js
+++ MERN Training/NODE/index.js
@@ -2,18 +2,63 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const todos = ["learn html","learn css", "learn php"];
+app.use(express.urlencoded())
+const todos = ["learn html", "learn css", "learn php"];
 
 app.get('/', (req, res) => {
-    let textToDisplay = "";
-    for (let i=0; i<todos.length; i++){
-        textToDisplay += `<li>${todos[i]}</li>`;
-    }
+  let textToDisplay = "";
+  todos.forEach((todo, index) => {
+    textToDisplay += `
+  <li>
+    ${todo}
+    <form method="post" action="/delete/${index}">
+    <button type = "submit">delete</button>
+    </form>
+    <a href="/edit/${index}">edit</a>
+  </li>`
+  });
 
-    res.send(`
-        <ul>${textToDisplay}</ul>`);
+  res.send(`
+      <form method="post" action="/add">
+      <input type="text" name="task" id="task" placeholder="your new task" />
+      <input type="submit" id="submit" value="Add" />
+      </form>
+      <ul>${textToDisplay}</ul>`);
+});
+
+app.get("/:index", (req, res) => {
+  const index = req.params.index;
+  res.send(todos[index]);
+});
+
+app.post("/add", (req, res) => {
+  // console.log(req.body);
+  // res.send("Form Submitted");
+  todos.push(req.body.task);
+  res.redirect("/");
+});
+
+app.post("/delete/:index", (req, res) => {
+  const index = req.params.index;
+  todos.splice(index, 1);
+  res.redirect("/");
+})
+
+app.get("/edit/:index", (req, res) => {
+  const index = req.params.index;
+  res.send(`
+    <form method="post" action="/edit/${index}">
+    <input type="text" name="task" id="task" value="${todos[index]}"/>
+    <input type="submit" id="submit" value="Update" />
+    </form>
+    `)
+})
+
+app.post("/edit/:index", (req, res) => {
+  todos[req.params.index] = req.body.task;
+  res.redirect("/");
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
