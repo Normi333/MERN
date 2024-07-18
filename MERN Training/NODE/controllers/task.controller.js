bcf6247fdd60eba3b340c061e566404b162c0bc6
Normi333 <shisirthapa146@gmail.com>
2024-07-18 17:03:58 +0545

    Bootstrapping index.ejs file

--- MERN Training/NODE/controllers/task.controller.js
+++ MERN Training/NODE/controllers/task.controller.js
@@ -1,29 +1,34 @@
 const Task = require("../models/Task");
 
 const getTasks = async (req, res) => {
-    const tasks = await Task.find();
+    if (!req.session.user) return res.redirect("/auth/sign-in");
+    const tasks = await Task.find({ user: req.session.user._id });
     res.render("index", { tasks });
 };
 
 const createTasks = async (req, res) => {
-    await Task.create({ title: req.body.title });
+    if (!req.session.user) return res.redirect("/auth/sign-in");
+    await Task.create({ title: req.body.title, user: req.session.user._id });
     res.redirect("/");
 };
 
 const deleteTasks = async (req, res) => {
+    if (!req.session.user) return res.redirect("/auth/sign-in");
     const taskId = req.params.id;
-    await Task.deleteOne({ _id: taskId });
+    await Task.deleteOne({ _id: taskId, user: req.session.user._id });
     res.redirect('/');
 };
 
 const editTasks = async (req, res) => {
+    if (!req.session.user) return res.redirect("/auth/sign-in");
     const taskId = req.params.id;
-    const task = await Task.findOne({ _id: taskId });
+    const task = await Task.findOne({ _id: taskId, user: req.session.user._id });
     //eslai ni ejs ma rakhne
     res.render("edit", { task });
 };
 
 const updateTasks = async (req, res) => {
+    if (!req.session.user) return res.redirect("/auth/sign-in");
     // todos[req.params.index] = req.body.task;
     const taskId = req.params.id;
     await Task.updateOne({ _id: taskId }, { title: req.body.title });
--- MERN Training/NODE/models/Task.js
+++ MERN Training/NODE/models/Task.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const { Schema } = require("mongoose");
 //creating schema of table
 const taskSchema = new mongoose.Schema({
     title: String,
     description: String,
+    user: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 //creating table
