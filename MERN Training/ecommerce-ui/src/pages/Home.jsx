ae6674b064763193d3f6b50c43e30904c5017a88
Normi333 <shisirthapa146@gmail.com>
2024-12-05 13:45:07 +0545

    Home

--- MERN Training/ecommerce-ui/src/pages/Home.jsx
+++ MERN Training/ecommerce-ui/src/pages/Home.jsx
@@ -4,7 +4,11 @@ import FeaturedProducts from "../components/FeaturedProducts";
 export default function Home() {
   return (
     <>
-      <img src="/banner.jpg" width="100%" />
+      <img
+        src="/banner.jpg"
+        width="100%"
+        style={{ padding: "3px 0px 3px 0px" }}
+      />
       <LatestProducts />
       <FeaturedProducts />
     </>
